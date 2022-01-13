b69f96597ba2e3199a1fcdfe53b5304ca3da17f5
suelleOliveiraTi <suellen.oliveira.geoit@gmail.com>
2022-01-13 16:41:37 -0300

    uptade js

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Crie uma variável que receba um nome;
+let nome = "Suellen"
+// Crie uma variável que receba um número;
+let numero = 15
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let anoAtual = 2022 - numero
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+console.log(numero + anoAtual)
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log("Estou mexendo no console.log;")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true;
+console.log(quartaFeira);
+let quartaFeira2 = false;
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log(typeof quartaFeira);
